697b5f562100ab91057e63a22c35b78dd76c38f9
sercanakdeniz <sercanakdeniz1997@gmail.com>
2020-07-25 20:12:59 +0300

    shareNote endpoint and it's service method

--- Noteapp/src/main/java/com/team5/Noteapp/Controller/NoteController.java
+++ Noteapp/src/main/java/com/team5/Noteapp/Controller/NoteController.java
@@ -3,50 +3,50 @@ package com.team5.Noteapp.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.team5.Noteapp.Entity.Note;
 import com.team5.Noteapp.Filter.AddPermissionFilter;
 import com.team5.Noteapp.Service.NoteService;
 
+@RequestMapping("/notes")
 @RestController
 public class NoteController {
-	
-	@Autowired									 
-	private NoteService noteService;				 
-	
-	@GetMapping("/notes/all")
+
+	@Autowired
+	private NoteService noteService;
+
+	@GetMapping("/all")
 	public List<Note> getAllNotes() {
 		return noteService.getAllNotes();
 	}
-	
+
 	@AddPermissionFilter(permissions = "read")
-	@GetMapping("/notes/{id}")
+	@GetMapping("/{id}")
 	public Note getNoteById(@PathVariable int id) {
 		return noteService.getNoteById(id);
 	}
-	
-	@PostMapping("/notes/add")
+
+	@PostMapping("/add")
 	public void addNote(@RequestBody Note note) {
 		noteService.addNote(note);
 	}
-	
+
 	@AddPermissionFilter(permissions = "write")
-	@PutMapping("/notes/edit/{id}")
-	public void editNote(@RequestBody Note note,@PathVariable int id) {
+	@PutMapping("/edit/{id}")
+	public void editNote(@RequestBody Note note, @PathVariable int id) {
 		noteService.editNote(note, id);
 	}
-	
-	@AddPermissionFilter(permissions = {"write","read"})
-	@DeleteMapping("/notes/delete/{id}")
+
+	@AddPermissionFilter(permissions = {"write", "read"})
+	@DeleteMapping("/delete/{id}")
 	public void deleteNote(@PathVariable Integer id) {
 		noteService.deleteNote(id);
 	}
-	
+
+	@AddPermissionFilter(permissions =  {"owner"})
+	@PostMapping("/share/{noteId}/{userId}/{role}")
+	public void shareNote(@PathVariable int noteId, @PathVariable int userId, @PathVariable int role) {
+		noteService.shareNote(noteId, userId, role);
+	}
 }
--- Noteapp/src/main/java/com/team5/Noteapp/Service/NoteService.java
+++ Noteapp/src/main/java/com/team5/Noteapp/Service/NoteService.java
@@ -2,6 +2,12 @@ package com.team5.Noteapp.Service;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
+
+import com.team5.Noteapp.Entity.Permission;
+import com.team5.Noteapp.Entity.User;
+import com.team5.Noteapp.Repository.PermissionRepository;
+import com.team5.Noteapp.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +19,12 @@ public class NoteService {
 	
 	@Autowired
 	private NoteRepository noteRepository;
+
+	@Autowired
+	private PermissionRepository permissionRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 	
 	public List<Note> getAllNotes() {
 		List <Note> notes = new ArrayList<Note>();
@@ -36,5 +48,31 @@ public class NoteService {
 	public void deleteNote(Integer id) {
 		noteRepository.deleteById(id);
 	}
-	
+
+	public void shareNote(int noteId, int userId, int role){
+		Optional<User> userOptional = userRepository.findById(userId);
+		if (userOptional.isPresent()){
+			Permission permission = new Permission();
+			permission.setNoteId(noteId);
+			permission.setUserId(userId);
+			if (role == 0){
+				permission.setRole("read");
+				permissionRepository.save(permission);
+			}
+			else if (role == 1){
+				permission.setRole("write");
+				permissionRepository.save(permission);
+			}
+			else if (role == 2){
+				Permission permission2 = new Permission();
+				permission2.setNoteId(noteId);
+				permission2.setUserId(userId);
+				permission2.setRole("read");
+				permissionRepository.save(permission2);
+				permission.setRole("write");
+				permissionRepository.save(permission);
+			}
+			else throw new IllegalArgumentException();
+		}
+	}
 }
